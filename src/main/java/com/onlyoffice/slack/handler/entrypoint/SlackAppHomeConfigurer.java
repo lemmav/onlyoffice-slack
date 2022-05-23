20bdc1bb45907593bdf8902ca4eadcdd54c8cacc
Dmitrii Vershinin <dmitry.vershinin@onlyoffice.com>
2022-05-23 18:30:18 +0500

    fix: app home view

--- src/main/java/com/onlyoffice/slack/handler/entrypoint/SlackAppHomeConfigurer.java
+++ src/main/java/com/onlyoffice/slack/handler/entrypoint/SlackAppHomeConfigurer.java
@@ -4,6 +4,7 @@ import com.onlyoffice.slack.SlackActions;
 import com.slack.api.bolt.App;
 import com.slack.api.methods.request.users.UsersInfoRequest;
 import com.slack.api.methods.response.users.UsersInfoResponse;
+import com.slack.api.model.User;
 import com.slack.api.model.block.LayoutBlock;
 import com.slack.api.model.event.AppHomeOpenedEvent;
 import lombok.RequiredArgsConstructor;
@@ -13,8 +14,7 @@ import org.springframework.context.annotation.Configuration;
 import java.util.ArrayList;
 import java.util.List;
 
-import static com.slack.api.model.block.Blocks.actions;
-import static com.slack.api.model.block.Blocks.section;
+import static com.slack.api.model.block.Blocks.*;
 import static com.slack.api.model.block.composition.BlockCompositions.markdownText;
 import static com.slack.api.model.block.composition.BlockCompositions.plainText;
 import static com.slack.api.model.block.element.BlockElements.button;
@@ -34,7 +34,6 @@ public class SlackAppHomeConfigurer {
                             .build()
                     );
 
-            //TODO: Error page
             if (!infoResponse.isOk())
                 return ctx.ack();
 
@@ -42,7 +41,7 @@ public class SlackAppHomeConfigurer {
                     .userId(payload.getEvent().getUser())
                     .view(view(view -> view
                             .type("home")
-                            .blocks(getBlocks(infoResponse.getUser().isAdmin()))
+                            .blocks(getBlocks(infoResponse.getUser()))
                     ))
                     .token(ctx.getBotToken())
             );
@@ -51,19 +50,31 @@ public class SlackAppHomeConfigurer {
         });
     }
 
-    //TODO: Proper UI
-    private List<LayoutBlock> getBlocks(boolean isAdmin) {
+    private List<LayoutBlock> getBlocks(User user) {
         List<LayoutBlock> blocks = new ArrayList<>();
 
-        blocks.add(section(section -> section
-                .text(markdownText(mt -> mt.text("BRUH")))
+        blocks.addAll(List.of(
+                header(h -> h.text(plainText("Welcome to ONLYOFFICE"))),
+                section(s -> s.text(markdownText(
+                        "This app enables users to edit office document from Slack messages using *ONLYOFFICE Docs*."
+                ))),
+                section(s -> s.text(markdownText(
+                        "The app allows to: \n*1. Edit* text documents, spreadsheets and presentations." +
+                        "\n*2. Share* files with basic permission types - viewing/editing.\n" +
+                        "*3. Co-edit* documents in real-time."
+                ))),
+                divider(),
+                context(List.of(
+                        markdownText("Logged in as <@"+user.getId()+">")
+                ))
         ));
 
-        if (isAdmin) {
+        if (user.isAdmin()) {
             blocks.add(actions(a -> a.elements(
                     List.of(
                             button(b -> b
-                                    .text(plainText("Document Server Options"))
+                                    .text(plainText("Configure connection settings"))
+                                    .style("primary")
                                     .actionId(SlackActions.OPEN_SETTINGS.getEntrypoint())
                             )
                     )
