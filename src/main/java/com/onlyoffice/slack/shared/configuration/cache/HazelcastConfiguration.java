44f3fa5feb9a72458673ae19f9665f2d5eaa867c
Dmitrii Vershinin <dmitry.vershinin@onlyoffice.com>
2025-08-26 19:02:51 +0500

    fix: move dynamic map into hazelcast.yml

--- src/main/java/com/onlyoffice/slack/shared/configuration/cache/HazelcastConfiguration.java
+++ src/main/java/com/onlyoffice/slack/shared/configuration/cache/HazelcastConfiguration.java
@@ -1,6 +1,5 @@
 package com.onlyoffice.slack.shared.configuration.cache;
 
-import com.hazelcast.config.*;
 import com.hazelcast.core.HazelcastInstance;
 import com.hazelcast.map.IMap;
 import com.onlyoffice.slack.shared.persistence.entity.id.InstallerUserId;
@@ -8,7 +7,6 @@ import com.onlyoffice.slack.shared.transfer.cache.DocumentSessionKey;
 import com.onlyoffice.slack.shared.transfer.cache.EditorSession;
 import com.slack.api.bolt.model.builtin.DefaultBot;
 import com.slack.api.bolt.model.builtin.DefaultInstaller;
-import jakarta.annotation.PostConstruct;
 import java.time.Instant;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
@@ -23,34 +21,7 @@ public class HazelcastConfiguration {
   private static final String DOCUMENT_KEYS_MAP_KEY = "keys";
   private static final String INSTALLERS_MAP_KEY = "installers";
 
-  private final HazelcastFileSessionMapStoreFactory mapStoreFactory;
   private final HazelcastInstance hazelcastInstance;
-
-  @PostConstruct
-  public void configureMapStore() {
-    if (!hazelcastInstance.getConfig().getMapConfigs().containsKey(DOCUMENT_KEYS_MAP_KEY)) {
-      var fileSessionsMapConfig = new MapConfig(DOCUMENT_KEYS_MAP_KEY);
-      var fileSessionsEvictionConfig =
-          new EvictionConfig()
-              .setEvictionPolicy(EvictionPolicy.LRU)
-              .setMaxSizePolicy(MaxSizePolicy.USED_HEAP_SIZE)
-              .setSize(100);
-
-      var mapStoreConfig = new MapStoreConfig();
-
-      fileSessionsMapConfig.setEvictionConfig(fileSessionsEvictionConfig);
-
-      mapStoreConfig.setEnabled(true);
-      mapStoreConfig.setFactoryImplementation(mapStoreFactory);
-      mapStoreConfig.setWriteDelaySeconds(0);
-      mapStoreConfig.setWriteBatchSize(1);
-      mapStoreConfig.setWriteCoalescing(true);
-
-      fileSessionsMapConfig.setMapStoreConfig(mapStoreConfig);
-
-      hazelcastInstance.getConfig().addMapConfig(fileSessionsMapConfig);
-    }
-  }
 
   @Bean
   public IMap<String, Instant> states(final HazelcastInstance instance) {
